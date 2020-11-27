0bad725b5a982793160bcd2ce4eb4a2f02ffcb17
yoston <yostoncornet@hotmail.es>
2020-11-27 12:07:02 -0500

    comienzo

---
+++ app/src/main/java/com/example/examenpractico/Bienvenidos.java
@@ -0,0 +1,14 @@
+package com.example.examenpractico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bienvenidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenidos);
+    }
+}
